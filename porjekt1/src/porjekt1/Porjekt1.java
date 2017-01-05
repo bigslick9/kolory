8988d1c5a57bfa8eb5fa0bf00be017bc31216171
student <student@240-07.INFORMATYKA.local>
2017-01-05 11:15:14 +0100

    ekstrakcja kolorow

--- porjekt1/src/porjekt1/Porjekt1.java
+++ porjekt1/src/porjekt1/Porjekt1.java
@@ -16,6 +16,8 @@ import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
 import java.io.*;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
 import java.net.*;
 import javax.imageio.*;
 
@@ -113,10 +115,76 @@ public class Porjekt1 extends JFrame {
         int r = (rgb & 0x00ff0000) >>> 16;
         int g = (rgb & 0x0000ff00) >>> 8;
         int b = rgb & 0x000000ff;
-        gray = (int) (0.299 * r + 0.587 * g + 0.114 * b);
+        gray = (int) (0.299 * r + 0.587 * g + 0.114 * b); 
         return gray;
     }
+    
+    private static int Igrek(int r, int g, int b) {
+        int Y;
 
+        Y = (int) (0.299 * r + 0.587 * g + 0.114 * b); 
+        return Y;
+    }
+    
+    private static int Ju(int r, int g, int b) {
+        int U;
+        U = (int) (-0.147 * r - 0.289 * g + 0.436 * b); 
+        return U;
+    }
+    
+    private static int Fał(int r, int g, int b) {
+        int V;
+        V = (int) (0.615 * r - 0.515 * g - 0.1 * b); 
+        return V;
+    }
+    
+    private static int Igrek2(int Y, double alfa){
+        int y;
+        y=(int)round(Y/(22.5+alfa));
+        return y;
+    }
+    
+     private static int Ju2(int U, double alfa){
+        int u;
+        u=(int)round((U+111.18)/(22.236+alfa));
+        return u;
+    }
+    
+      private static int Fał2(int V, double alfa){
+        int v;
+        v=(int)round((V+156.825)/(31.365+alfa));
+        return v;
+    }
+      
+       private static void Processing(BufferedImage img)
+ {
+ int w=img.getWidth(null);
+ int h=img.getHeight(null);
+ for(int x=0;x<w;x++)
+ for(int y=0;y<h;y++)
+ {
+ int rgb=img.getRGB(x, y);
+ int a=(rgb&0xff000000)>>>24;
+ int r=(rgb&0x00ff0000)>>>16;
+ int g=(rgb&0x0000ff00)>>>8;
+ int b=rgb&0x000000ff;
+
+      
+      int rr=121;
+      int gg=67;
+      int bb=116;
+      int RGB=0;
+      double alfa=1;
+      if(Igrek2(Igrek(r,g,b),alfa)==Igrek2(Igrek(rr,gg,bb),alfa) && Ju2(Ju(r,g,b),alfa)==Ju2(Ju(rr,gg,bb),alfa) && Fał2(Fał(r,g,b),alfa)==Fał2(Fał(rr,gg,bb),alfa) )
+        //zapis kanałów
+      
+ RGB=b|(g<<8)|(r<<16)|(a<<24);
+ img.setRGB(x, y, RGB);
+      
+ }     
+ }
+       
+       
     private static BufferedImage copyImage(BufferedImage source) {
         BufferedImage b = new BufferedImage(source.getWidth(),
                 source.getHeight(), source.getType());
@@ -151,25 +219,25 @@ public class Porjekt1 extends JFrame {
         return a;
     }
 
-    private static void Processing(BufferedImage img) {
-        int w = img.getWidth(null);
-        int h = img.getHeight(null);
-
-        BufferedImage temp = copyImage(img);
-        int[][] maska = new int[][]{
-            {-5, 3, 3},
-            {-5, 0, 3},
-            {-5, 3, 3}};
-
-        for (int x = 1; x < w-1; x++) {
-            for (int y = 1; y < h-1; y++) {
-                int a = filterPixel(temp, x, y, maska);
-                int RGB = a | (a << 8) | (a << 16) | (a << 24);
-                img.setRGB(x, y, RGB);
-            }
-        }
-
-    }
+//    private static void Processing(BufferedImage img) {
+//        int w = img.getWidth(null);
+//        int h = img.getHeight(null);
+//
+//        BufferedImage temp = copyImage(img);
+//        int[][] maska = new int[][]{
+//            {-5, 3, 3},
+//            {-5, 0, 3},
+//            {-5, 3, 3}};
+//
+//        for (int x = 1; x < w-1; x++) {
+//            for (int y = 1; y < h-1; y++) {
+//                int a = filterPixel(temp, x, y, maska);
+//                int RGB = a | (a << 8) | (a << 16) | (a << 24);
+//                img.setRGB(x, y, RGB);
+//            }
+//        }
+//
+//    }
 
     public static void main(String args[]) {
         JFrame frame = new Porjekt1();
